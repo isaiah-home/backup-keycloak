0f0375a7c464093573b76b44db1028dd69da58ef
isaiah-v <isaiah.v@comcast.net>
2022-05-15 12:05:26 -0700

    Added keycloak  restore

--- restore/restore.sh
+++ restore/restore.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+if [ -z ${1+x} ]; then echo "s3 restore file not defined"; exit 1; fi
+
 if [ -z ${MYSQL_HOST+x} ]; then echo "MYSQL_HOST not set"; exit 1; fi
 if [ -z ${MYSQL_PORT+x} ]; then echo "MYSQL_PORT not set"; exit 1; fi
 if [ -z ${MYSQL_USERNAME+x} ]; then echo "MYSQL_USERNAME not set"; exit 1; fi
@@ -9,19 +11,15 @@ if [ -z ${AWS_ACCESS_KEY_ID+x} ]; then echo "AWS_ACCESS_KEY_ID not set"; exit 1;
 if [ -z ${AWS_SECRET_ACCESS_KEY+x} ]; then echo "AWS_SECRET_ACCESS_KEY not set"; exit 1; fi
 if [ -z ${AWS_DEFAULT_REGION+x} ]; then echo "AWS_DEFAULT_REGION not set"; exit 1; fi
 
-export BACKUP_NAME=keycloak
-export BACKUP_BAK=$BACKUP_NAME.bak
-export BACKUP_ZIP=$BACKUP_NAME-$(date +'%Y-%m-%dT%H:%M:%S%z').zip
+# Pull restore file
+aws s3 cp s3://backups.ivcode.org/keycloak/$1 $1 || exit 1
 
-# Dump database to file
-(mysqldump --host=$MYSQL_HOST --port=$MYSQL_PORT --user=$MYSQL_USERNAME --password=$MYSQL_PASSWORD keycloak || exit 1) > $BACKUP_BAK
+# Unzip restore file
+unzip $1 || exit 1
 
-# Zip file
-zip $BACKUP_ZIP $BACKUP_BAK || exit 1
+# Restore database
+mysql --host=$MYSQL_HOST --port=$MYSQL_PORT --user=$MYSQL_USERNAME --password=$MYSQL_PASSWORD --database=keycloak < keycloak.bak
 
-# Save zip to s3
-aws s3 cp $BACKUP_ZIP s3://backups.ivcode.org/keycloak/$BACKUP_ZIP || exit 1
-
-# Cleanup
-rm $BACKUP_BAK
-rm $BACKUP_ZIP
+#Cleanup
+rm keycloak.bak
+rm $1
