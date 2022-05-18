e62f0e35d977d6018a1112e8feacb146aa09e6c0
isaiah-v <isaiah.v@comcast.net>
2022-05-17 19:49:09 -0700

    removed terraform added deploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p ~/bin
+cp scripts/keycloak-backup.sh ~/bin/keycloak-backup.sh
+cp scripts/keycloak-restore.sh ~/bin/keycloak-restore.sh
+cp scripts/nextcloud-backup.sh ~/bin/nextcloud-backup.sh
+cp scripts/nextcloud-restore.sh ~/bin/nextcloud-restore.sh
+cp scripts/wikijs-backup.sh ~/bin/wikijs-backup.sh
+cp scripts/wikijs-restore.sh ~/bin/wikijs-restore.sh
+
